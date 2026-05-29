376ddf91858438c78e2add927309fb4b5f3aa11b
johkrsj <mtabd5yly@gmail.com>
2026-05-29 16:29:27 +0300

    Update page.jsx

--- app/    profile/page.jsx
+++ app/    profile/page.jsx
@@ -1,90 +1,49 @@
 "use client";
 
-export default function ProfilePage() {
+export default function Home() {
+
+  function OpenProfile() {
+    window.location.href = "/profile";
+  }
+
   return (
     <div
       style={{
-        backgroundColor: "#1f1f1f",
+        backgroundColor: "#0a0a0a",
         minHeight: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
         color: "white",
         fontFamily: "Arial",
-        padding: "20px",
       }}
     >
-      <div
+      <h1
         style={{
-          display: "flex",
-          alignItems: "center",
-          gap: "18px",
-          marginTop: "30px",
+          fontSize: "60px",
+          marginBottom: "30px",
+          letterSpacing: "-2px",
         }}
       >
-        <div
-          style={{
-            width: "90px",
-            height: "90px",
-            borderRadius: "50%",
-            backgroundColor: "#d9d9d9",
-          }}
-        />
-
-        <div>
-          <h1
-            style={{
-              margin: 0,
-              fontSize: "32px",
-            }}
-          >
-            Username
-          </h1>
-
-          <p
-            style={{
-              color: "#b0b0b0",
-              marginTop: "8px",
-              fontSize: "18px",
-            }}
-          >
-            Bio
-          </p>
-        </div>
-      </div>
+        JOHK
+      </h1>
 
       <button
+        onClick={OpenProfile}
         style={{
-          marginTop: "30px",
-          width: "100%",
-          padding: "14px",
+          padding: "16px 34px",
           borderRadius: "18px",
           border: "none",
-          backgroundColor: "#2a2a2a",
-          color: "white",
-          fontSize: "16px",
+          backgroundColor: "white",
+          color: "black",
+          fontSize: "18px",
           fontWeight: "600",
+          cursor: "pointer",
         }}
       >
-        Edit Profile
+        Enter
       </button>
-
-      <div
-        style={{
-          display: "grid",
-          gridTemplateColumns: "1fr 1fr 1fr",
-          gap: "8px",
-          marginTop: "30px",
-        }}
-      >
-        {[1,2,3,4,5,6].map((item) => (
-          <div
-            key={item}
-            style={{
-              aspectRatio: "1 / 1",
-              backgroundColor: "#2a2a2a",
-              borderRadius: "22px",
-            }}
-          />
-        ))}
-      </div>
     </div>
   );
 }
