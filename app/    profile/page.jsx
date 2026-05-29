8abfb113ad583e48d37731040e83b768bd9c53e8
johkrsj <mtabd5yly@gmail.com>
2026-05-29 14:25:59 +0300

    Update page.jsx

--- app/    profile/page.jsx
+++ app/    profile/page.jsx
@@ -1,116 +1,31 @@
-"use client";
-
 export default function ProfilePage() {
   return (
     <div
       style={{
         backgroundColor: "#000",
+        color: "#fff",
         minHeight: "100vh",
-        color: "white",
-        padding: "30px",
+        padding: "40px",
         fontFamily: "sans-serif",
       }}
     >
-      <div
+      <h1
         style={{
-          display: "flex",
-          alignItems: "center",
-          gap: "20px",
-          marginBottom: "30px",
+          fontSize: "48px",
+          marginBottom: "20px",
         }}
       >
-        <img
-          src="https://i.pravatar.cc/150"
-          alt="profile"
-          style={{
-            width: "90px",
-            height: "90px",
-            borderRadius: "50%",
-          }}
-        />
+        Profile
+      </h1>
 
-        <div>
-          <h1
-            style={{
-              fontSize: "32px",
-              marginBottom: "10px",
-            }}
-          >
-            @gojo
-          </h1>
-
-          <p
-            style={{
-              color: "#aaa",
-            }}
-          >
-            Welcome to my Johk profile 
-          </p>
-        </div>
-      </div>
-
-      <div
+      <p
         style={{
-          display: "flex",
-          gap: "20px",
-          marginBottom: "40px",
+          color: "#999",
+          fontSize: "20px",
         }}
       >
-        <div>
-          <strong>12</strong>
-          <p style={{ color: "#999" }}>Posts</p>
-        </div>
-
-        <div>
-          <strong>530</strong>
-          <p style={{ color: "#999" }}>Followers</p>
-        </div>
-
-        <div>
-          <strong>120</strong>
-          <p style={{ color: "#999" }}>Following</p>
-        </div>
-      </div>
-
-      <div
-        style={{
-          display: "grid",
-          gridTemplateColumns: "1fr 1fr",
-          gap: "12px",
-        }}
-      >
-        <div
-          style={{
-            backgroundColor: "#111",
-            height: "160px",
-            borderRadius: "16px",
-          }}
-        />
-
-        <div
-          style={{
-            backgroundColor: "#111",
-            height: "160px",
-            borderRadius: "16px",
-          }}
-        />
-
-        <div
-          style={{
-            backgroundColor: "#111",
-            height: "160px",
-            borderRadius: "16px",
-          }}
-        />
-
-        <div
-          style={{
-            backgroundColor: "#111",
-            height: "160px",
-            borderRadius: "16px",
-          }}
-        />
-      </div>
+        Welcome To Johk
+      </p>
     </div>
   );
 }
