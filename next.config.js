952aeec228e0f2fc6a5daa25ae7b7e36d6c93e0d
johkrsj <mtabd5yly@gmail.com>
2026-06-02 19:45:16 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-module.exports = nextConfig
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/auth',
+        permanent: true,
+      },
+    ]
+  },
+}
